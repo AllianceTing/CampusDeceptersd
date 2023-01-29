fc3ae8339348cf488578ee819d730c73405cbff3
张震霆 <2426446427@qq.com>
2023-01-29 14:19:27 +0800

    menf

---
+++ Toc/UserLogin/src/main/java/com/User/LoginApplication.java
@@ -0,0 +1,17 @@
+package com.User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PROJECT_NAME LoginApplication
+ *
+ * @author Alliance github_https://github.com/AllianceTing
+ * DATE 2023/1/29~14:14
+ */
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
